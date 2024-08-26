f912c68e226b1f439c4f3b16f86b7acd9471e8f5
Heloysa <134120126+wineberryyy@users.noreply.github.com>
2024-08-25 21:35:30 -0300

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,19 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        return None
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+s = Stack()
+s.push(1)
+s.push(2)
+print(s.pop())  
