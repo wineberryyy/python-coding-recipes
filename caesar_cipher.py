452649eeac2d0b7d55fad15ed45f2dbedbbb61fb
Heloysa <134120126+wineberryyy@users.noreply.github.com>
2024-08-25 21:35:03 -0300

    Create caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,14 @@
+def caesar_cipher(text, shift):
+    result = ""
+
+    for char in text:
+        if char.isalpha():
+            shift_base = 65 if char.isupper() else 97
+            result += chr((ord(char) - shift_base + shift) % 26 + shift_base)
+        else:
+            result += char
+
+    return result
+
+print(caesar_cipher("HELLO WORLD", 3))  
+# expected: KHOOR ZRUOG
