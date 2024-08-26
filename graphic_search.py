4733f1d6bfea5fe43832faa9291505488b72c95a
Heloysa <134120126+wineberryyy@users.noreply.github.com>
2024-08-25 21:31:24 -0300

    Create graphic_search.py

---
+++ graphic_search.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            print(vertex, end=" ")
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
+
+bfs(graph, 'A')  
